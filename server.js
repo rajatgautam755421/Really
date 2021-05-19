888405881c9c1da061839be6ce9a862782d90b55
rajat755421 <rajatgautam832@gmail.com>
2021-05-19 21:01:34 +0545

    12th commit

--- server.js
+++ server.js
@@ -26,12 +26,13 @@ const config = {
   baseURL: "https://helloappnotruth.herokuapp.com/",
   clientID: "yRccnJkmTlNYPk0yxbz1YAO6JKVjq5k6",
   issuerBaseURL: "https://dev-jr0iokjh.us.auth0.com",
+  redirect_uri: "https://helloappnotruth.herokuapp.com/",
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-app.get("/", (req, res) => {
+app.get("/", requiresAuth(), (req, res) => {
   if (req.oidc.isAuthenticated()) {
     res.render("index", { data1: req.oidc.user });
   } else {
