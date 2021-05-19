2a262b03e0a68b89a89dca9e475b5d3dce668d68
rajat755421 <rajatgautam832@gmail.com>
2021-05-19 17:13:49 +0545

    Tenth Commit

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
 const contactModel = require("../models/contactSchema");
 const nodeMailer = require("nodemailer");
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "lfkmnvjknrjibghbrt",
+  baseURL: "http://localhost:3000",
+  clientID: "yRccnJkmTlNYPk0yxbz1YAO6JKVjq5k6",
+  issuerBaseURL: "https://dev-jr0iokjh.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
+
 router.get("/", (req, res) => {
-  res.render("contact", { name: "aman" });
+  if (req.oidc.isAuthenticated()) {
+    res.render("contact");
+  } else {
+    res.redirect("/login");
+  }
 });
 router.post("/", async (req, res) => {
   const user = new contactModel({
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const contactRoute = require("./routes/contactRoute");
-const Port = process.env.PORT || 7000;
+const { auth } = require("express-openid-connect");
+const { requiresAuth } = require("express-openid-connect");
+
+const Port = process.env.PORT || 3000;
 
 //Parsing the data
 app.use(express.urlencoded({ extended: false }));
@@ -10,10 +13,35 @@ app.use(express.json());
 //View Engine
 app.set("view engine", "hbs");
 //Main route
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 app.use("/contact", contactRoute);
+
+//Auth0 authentication
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "lfkmnvjknrjibghbrt",
+  baseURL: "https://helloappnotruth.herokuapp.com/",
+  clientID: "yRccnJkmTlNYPk0yxbz1YAO6JKVjq5k6",
+  issuerBaseURL: "https://dev-jr0iokjh.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+app.get("/", (req, res) => {
+  if (req.oidc.isAuthenticated()) {
+    res.render("index");
+  } else {
+    res.redirect("/login");
+  }
+});
+// profile
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(req.oidc.user);
+});
 
 mongoose.connect(
   "mongodb+srv://rajat:rajat12345@cluster0.d6wqf.mongodb.net/test",
