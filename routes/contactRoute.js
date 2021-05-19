cee716fdcdbcd41c17dec648b12cb45064f73dd2
rajat755421 <rajatgautam832@gmail.com>
2021-05-19 13:11:37 +0545

    Eighth Commit

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const contactModel = require("../models/contactSchema");
-
+const nodeMailer = require("nodemailer");
 router.get("/", (req, res) => {
   res.render("contact", { name: "aman" });
 });
@@ -13,10 +13,32 @@ router.post("/", async (req, res) => {
   });
   try {
     const savedContact = await user.save();
-    res.json(savedContact);
+    res.redirect("/contact");
   } catch (error) {
     res.send(error);
   }
+
+  var transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "gautamrajat185@gmail.com",
+      pass: 1462962708,
+    },
+  });
+  var mailOptions = {
+    from: "gautamrajat185@gmail.com",
+    to: `${req.body.email}`,
+    subject: "Contact  us through Emalis",
+    text: `Thankyou ${req.body.name} for contacting us.We will react you soon.`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 });
 
 module.exports = router;
